7f6ba3a4d38aaf35fc1d866a21c96b515fd0ae75
Thomas Eubank <eubanktr@gmail.com>
2021-09-01 12:40:04 -0400

    deploying

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.use(require("./routes/api.js"));
