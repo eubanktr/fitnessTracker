98536252ba98a0085c982cfc8230a72e85f8648c
Thomas Eubank <eubanktr@gmail.com>
2021-08-31 20:12:46 -0400

    pushing work.. added models/routes/server/package/.env

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path"); // not sure if needed?
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/home.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
